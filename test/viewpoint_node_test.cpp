9f39b7a609cc231cd949c8c1f825b06c3ab4ef5e
Justin Huang <jstn@cs.washington.edu>
2014-05-01 15:39:41 -0700

    Added tests on marker rotations.

--- test/viewpoint_node_test.cpp
+++ test/viewpoint_node_test.cpp
@@ -61,6 +61,60 @@ TEST(TestViewpointNode, ZControl) {
   EXPECT_FLOAT_EQ(marker_position.y + sqrt(1.5), result_point.y);
   EXPECT_FLOAT_EQ(marker_position.z, result_point.z);
 }
+
+TEST(TestViewpointNode, Pitch) {
+  geometry_msgs::Point current_position;
+  current_position.x = 2;
+  current_position.y = 2;
+  current_position.z = 2;
+  geometry_msgs::Point marker_position;
+  marker_position.x = 1;
+  marker_position.y = 1;
+  marker_position.z = 1;
+  auto control_type = MarkerEvent::PITCH;
+  geometry_msgs::Point result_point;
+  ComputeOrthogonalPoint(current_position, marker_position, control_type,
+                         &result_point);
+  EXPECT_FLOAT_EQ(marker_position.x, result_point.x);
+  EXPECT_FLOAT_EQ(marker_position.y + sqrt(3), result_point.y);
+  EXPECT_FLOAT_EQ(marker_position.z, result_point.z);
+}
+
+TEST(TestViewpointNode, Roll) {
+  geometry_msgs::Point current_position;
+  current_position.x = 2;
+  current_position.y = 2;
+  current_position.z = 2;
+  geometry_msgs::Point marker_position;
+  marker_position.x = 1;
+  marker_position.y = 1;
+  marker_position.z = 1;
+  auto control_type = MarkerEvent::ROLL;
+  geometry_msgs::Point result_point;
+  ComputeOrthogonalPoint(current_position, marker_position, control_type,
+                         &result_point);
+  EXPECT_FLOAT_EQ(marker_position.x + sqrt(3), result_point.x);
+  EXPECT_FLOAT_EQ(marker_position.y, result_point.y);
+  EXPECT_FLOAT_EQ(marker_position.z, result_point.z);
+}
+
+TEST(TestViewpointNode, Yaw) {
+  geometry_msgs::Point current_position;
+  current_position.x = 2;
+  current_position.y = 2;
+  current_position.z = 2;
+  geometry_msgs::Point marker_position;
+  marker_position.x = 1;
+  marker_position.y = 1;
+  marker_position.z = 1;
+  auto control_type = MarkerEvent::YAW;
+  geometry_msgs::Point result_point;
+  ComputeOrthogonalPoint(current_position, marker_position, control_type,
+                         &result_point);
+  EXPECT_FLOAT_EQ(marker_position.x, result_point.x);
+  EXPECT_FLOAT_EQ(marker_position.y, result_point.y);
+  EXPECT_FLOAT_EQ(marker_position.z + sqrt(3), result_point.z);
+}
 }
 
 int main(int argc, char **argv){
