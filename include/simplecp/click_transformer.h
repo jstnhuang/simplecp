e3b873c12a617ecb1e1753cb7bf7e5741f765b2d
Justin Huang <jstn@cs.washington.edu>
2014-04-30 18:46:07 -0700

    Populate control_type field of the MarkerEvent.

--- include/simplecp/click_transformer.h
+++ include/simplecp/click_transformer.h
@@ -1,11 +1,15 @@
 #ifndef SIMPLECP_CLICK_TRANSFORMER_H_
 #define SIMPLECP_CLICK_TRANSFORMER_H_
 
+#include <map>
+
 #include <ros/node_handle.h>
 #include <ros/publisher.h>
 #include <ros/subscriber.h>
 #include <ros/time.h>
 #include <visualization_msgs/InteractiveMarkerFeedback.h>
+
+#include "simplecp/MarkerEvent.h"
 
 namespace simplecp {
 enum class ClickState {
@@ -13,9 +17,14 @@ enum class ClickState {
   kDown
 };
 
+typedef std::map<std::string, std::string> ControlMap;
+
 class ClickTransformer {
  public:
   static const int kClickNanoseconds = 100000000;
+  static const ControlMap kGripperControls;
+  static const ControlMap kPointHeadControls;
+
   ClickTransformer(const std::string& feedback_topic,
                    const std::string& event_topic);
 
@@ -28,6 +37,8 @@ class ClickTransformer {
 
   void MarkerCallback(
       const visualization_msgs::InteractiveMarkerFeedback& feedback);
+  std::string GetControlType(const std::string& marker_name,
+                             const std::string& control_name);
 };
 }  // namespace simplecp
 
--- src/click_transformer.cpp
+++ src/click_transformer.cpp
@@ -1,10 +1,29 @@
 #include "simplecp/click_transformer.h"
 
+#include <ros/console.h>
 #include <ros/duration.h>
 
-#include "simplecp/MarkerEvent.h"
-
 namespace simplecp {
+
+// Maps built by observing /pr2_marker_control_transparent/feedback
+// TODO(jstn): These sometimes change between executions :(.
+const ControlMap ClickTransformer::kGripperControls = {
+  { "_u0", MarkerEvent::X },
+  { "_u4", MarkerEvent::Y },
+  { "_u2", MarkerEvent::Z },
+  { "_u3", MarkerEvent::PITCH },
+  { "", MarkerEvent::ROLL },
+  { "_u1", MarkerEvent::YAW }
+};
+
+const ControlMap ClickTransformer::kPointHeadControls = {
+  { "_u1", MarkerEvent::X },
+  { "_u5", MarkerEvent::Y },
+  { "_u3", MarkerEvent::Z },
+  { "_u4", MarkerEvent::PITCH },
+  { "_u0", MarkerEvent::ROLL },
+  { "_u2", MarkerEvent::YAW }
+};
 
 ClickTransformer::ClickTransformer(const std::string& feedback_topic,
                                    const std::string& event_topic)
@@ -36,22 +55,42 @@ void ClickTransformer::MarkerCallback(
     if (feedback.event_type
         == visualization_msgs::InteractiveMarkerFeedback::MOUSE_UP) {
       auto click_time = ros::Time::now() - mouse_down_time_;
+      simplecp::MarkerEvent event;
+      event.marker_name = feedback.marker_name;
+      event.control_type = GetControlType(feedback.marker_name,
+                                          feedback.control_name);
       if (click_time < ros::Duration(0, kClickNanoseconds)) {
-        click_state_ = ClickState::kStart;
-        simplecp::MarkerEvent event;
         event.type = simplecp::MarkerEvent::CLICK;
-        event.marker_name = feedback.marker_name;
-        event_publisher_.publish(event);
       } else {
-        click_state_ = ClickState::kStart;
-        simplecp::MarkerEvent event;
         event.type = simplecp::MarkerEvent::DRAG;
-        event.marker_name = feedback.marker_name;
-        event_publisher_.publish(event);
       }
+      event_publisher_.publish(event);
+      click_state_ = ClickState::kStart;
     }
   } else {
     click_state_ = ClickState::kStart;
+  }
+}
+
+std::string ClickTransformer::GetControlType(const std::string& marker_name,
+                                             const std::string& control_name) {
+  try {
+    if (marker_name == "head_point_goal") {
+      return kPointHeadControls.at(control_name);
+    } else if (marker_name == "l_gripper_control"
+        || marker_name == "r_gripper_control") {
+      return kGripperControls.at(control_name);
+    } else if (marker_name == "l_posture_control"
+        || marker_name == "r_gripper_control") {
+      return MarkerEvent::ROLL;
+    } else {
+      ROS_WARN("Unknown marker %s", marker_name.c_str());
+      return "Unknown";
+    }
+  } catch (const std::out_of_range& e) {
+    ROS_ERROR("Unknown control %s for marker %s", control_name.c_str(),
+              marker_name.c_str());
+    return "Unknown";
   }
 }
 }  // namespace simplecp
